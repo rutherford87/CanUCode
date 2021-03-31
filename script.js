1a1ff05dfabc4653041ed856bbbb32fd3fccb451
Tyler Rutherford <rutherford87@gmail.com>
2021-03-30 22:58:59 -0500

    Pushing as a minimum value product - quiz will run 3 questions. Need to add timer and high score local storage

--- script.js
+++ script.js
@@ -6,15 +6,10 @@ var choice1 = document.getElementById('choice1');
 var choice2 = document.getElementById('choice2');
 var choice3 = document.getElementById('choice3');
 var choice4 = document.getElementById('choice4');
+var scoreArea = document.getElementById('update');
 //still need vars for timer  
 //var timeLeftEl= document.querySelector(".timeLeft") 
-
-//Begin function for taking quiz
-//insert intervalTimer (check wordguess game)
-//if time ends, display game over
-//timeLeftEl.textContent = "Time left " + secondsLeft
-//else {
-
+//var timeLimit = 10 // 10 seconds
 //Build the question, answer choice, and correct answer array all as one, could fill out with more later
 var questionAnswer = [
 	{
@@ -23,33 +18,43 @@ var questionAnswer = [
 		choice2: 'Austin',
 		choice3: 'Denver',
 		choice4: 'Dallas',
-		correctAnswer: '2'
+		correctAnswer: 'choice2'
 	},{
-		question: "What is the best Texas BBQ?",
+		question: "Who has the best BBQ?",
 		choice1: 'Snows',
-		choice2: 'Mammas',
+		choice2: 'Mamma',
 		choice3: 'Franklins',
 		choice4: 'Terry Blacks',
-		correctAnswer: '2'
+		correctAnswer: 'choice2'
+	},{
+		question: "Who has the best beer to go?",
+		choice1: 'PHP',
+		choice2: 'St Elmo',
+		choice3: 'Rather have a topo',
+		choice4: 'Tap... water',
+		correctAnswer: 'choice2'
 	}
 ];
+
 //Set up Variables
 //index will track where in the quiz out of number of questions the user is currently.
 var index = 0;
-var lastQuestion = questionAnswer.length-1;
 //score will start at zero and add 1 as each answer is selected correctly. This will be diplayed and logged to local storage when the quiz has ended
 var score = 0;
-//Timer variables need to include
-//var timeLimit = 10 // 10 seconds
 
-//function to display question 1
+//function to display question 1 through last question, after last question is complete, display final score
 function showQuestion(){
+	if (index <= (questionAnswer.length-1)) {
 	question.textContent=questionAnswer[index].question;
 	choice1.textContent=questionAnswer[index].choice1;
 	choice2.textContent=questionAnswer[index].choice2;
 	choice3.textContent=questionAnswer[index].choice3;
 	choice4.textContent=questionAnswer[index].choice4;
-}
+	scoreArea.textContent='Score: ' + score;
+} else {
+	questionArea.textContent="Congrats, you finished!";
+	scoreArea.textContent='Final Score: ' + score;
+}}
 
 //function to start quiz with a click of a button
 start.addEventListener("click",startQuiz);
@@ -58,25 +63,26 @@ function startQuiz(){
 	start.style.display="none";
 	showQuestion();
 	questionArea.style.display="block"; // could change later
-	indexQuestion();
 }
 
 //After logging which selection was made (next if statements to end game if index=number of questions)
 //function to index to next question (position in array)
-
 function indexQuestion(){
-if (index <= (questionAnswer.length-1)) 
+if (index <= (questionAnswer.length-1)) {
 	index++;
+} 
 }
 
-//After a function 
-
-//showScore in id=update - question.textContent('You scored ' + score +points!')
-
-//don't forget:
-//high score
-
-
-///////STRETCH GOALS
-//correct answer is +2, incorrect is -1
-//correct answer is +time
+//After a slection is made, write function to compare selection to answer
+function checkAnswer(selection){
+console.log(selection);
+console.log(questionAnswer[index].correctAnswer);
+	if(selection === questionAnswer[index].correctAnswer) {
+		score++; 
+		indexQuestion();
+		showQuestion();
+ } else {
+	indexQuestion();
+	showQuestion();
+}
+}
